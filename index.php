6395b0dbd38ff4fc0b1787d923b618c532b50079
daylanKifky <brunolaurencich@gmail.com>
2017-01-24 16:04:13 +0100

    first layout

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <html>
    <head>
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <link rel="stylesheet" href="Interactive_Form/i-form.css">
       <script
            src="https://code.jquery.com/jquery-3.1.1.min.js"
            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
@@ -19,11 +21,13 @@
          <div class="row">
             <div class="col-sm-12">
                <h1>Interactive content loader</h1>
+               <form id="i-form" action="" method="POST" enctype="multipart/form-data">
                <div class="row">
-                  <div class="sm-col-3" id='if-logo'> <!-- LOGO -->
+                  <div class="col-xs-3" id='if-logo'> <!-- LOGO -->
+                     <input type="file" name="image" />
                      
                   </div>
-                  <div class="sm-col-9" id='if-header'> <!-- HEADER -->
+                  <div class="col-xs-9" id='if-header'> <!-- HEADER -->
                      <div id="if-header-title">
                         
                      </div>
@@ -57,8 +61,6 @@
                   </div>
                </dir>
 
-               <form action="" method="POST" enctype="multipart/form-data">
-                  <input type="file" name="image" />
                   <input type="submit"/>
                </form>
                
